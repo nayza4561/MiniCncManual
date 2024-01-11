c7575cd7a1e3c5db7d847c82e1722b2dafd01d76
nayza4561 <nay290648@gmail.com>
2024-01-11 15:25:46 +0700

    first commit

---
+++ components/nav.php
@@ -0,0 +1,10 @@
+<nav class="d-flex flex-row justify-content-around align-items-center">
+    <a href="index.php" class="text-decoration-none text-light">
+        <h2>หน้าหลัก</h2>
+        <div class="d-flex align-items-center">
+            <a href="#" class="menu-nav">Contact</a>
+            <a href="#" class="menu-nav">Menu1</a>
+            <a href="#" class="menu-nav">Menu1</a>
+        </div>
+    </a>
+</nav>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <title>Project CNC</title>
+</head>
+
+<body>
+    <?php require("components/nav.php"); ?>
+    <!-- banner -->
+    <div class="indexBanner">
+        <h1>ยินดีต้อนรับเข้าสู่เว็บไซต์คู่มือการใช้งานเครื่อง Mini CNC</h1>
+        <button class="indexBannerbtn">Start</button>
+    </div>
+    <div class="container px-5 my-4 text-center">
+        <h2>วัตถุประสงค์ของการทำเว็บไซต์นี้ขึ้น</h2>
+        <span class="indexAbout">เพื่อแนวทาง วิทยาทานในการปฎิบัติในการใช้เครื่อง Mini CNC เพื่อใช้ในการสร้างสรรค์ผลงานแกะสลักอัตโนมัติขนาดย่อม ด้วยเครื่อง MiniCNC ผู้ใช้งานใหม่ ทำไปใช้งาน ตามความต้องการได้อย่างถูกต้องแม่นยำและปลอดภัย
+            เพื่อการศึกษาความรู้ในการนำไปเผยแพร่วิธีการใช้ หรือใช้ในสื่อการเรียนการสอน ช่วยในการรวบรวมข้อมูลสำหรับผู้ที่สนใจศึกษาในเรื่องของการใช้เครื่อง Mini CNC เป็นอีกหนึ่งแหล่งข้อมูลที่ช่วยได้ไม่มากก็น้อย เพื่อความสะดวกสบายของผู้หาข้อมูล
+        </span>
+    </div>
+</body>
+
+</html>
